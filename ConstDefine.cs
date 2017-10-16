d1d73ec881c72d946bd71029967d318fb4489aa8
zmxlhlh <1936455932@qq.com>
2017-10-16 11:44:53 +0800

    添加
    A

---
+++ ConstDefine.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConstDefine  {
+
+	public static string UPDATEVIEW="Updateview";
+	public static string CHANGEDATA="Changedata";
+}
---
+++ Father.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Father : MonoBehaviour {
+	 void DisplayInfo(){
+		print ("father");
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
